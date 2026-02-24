e81cff573d82334b80338a54788f408d0b44617f
joaonunovalente <jnvalente@ua.pt>
2026-02-24 09:15:17 +0000

    Update Footer component to include logo and adjust logo size in CSS

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,14 +1,10 @@
+import logo from "../assets/images/logo/logo-color.svg";
+
 function Footer() {
   return (
     <footer className="footer py-3">
       <div className="container text-center">
-        <small className="copyright">
-          Designed with <span className="sr-only">love</span>
-          <i className="bi bi-heart-fill" style={{ color: '#DE5547' }}></i>
-          {' '}by{' '}
-          <a className="theme-link" href="http://themes.3rdwavemedia.com" target="_blank" rel="noreferrer">Xiaoying Riley</a>
-          {' '}for developers
-        </small>
+        <img className="logo-icon" src={logo} alt="Logo do Cantinas.pt" />
       </div>
     </footer>
   );
