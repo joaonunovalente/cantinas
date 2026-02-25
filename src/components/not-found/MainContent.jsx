9bbc7209710c8b1f5614ed950b5d980e8bfd1338
joaonunovalente <jnvalente@ua.pt>
2026-02-25 09:08:21 +0000

    [404 page] Refactor MainContent component to NotFoundPage
    Every component '404 page' can now be found in 'components/not-found' folder

---
+++ src/components/not-found/MainContent.jsx
@@ -0,0 +1,14 @@
+function MainContent() {
+  return (
+      <section className="help-featured-section theme-section">
+        <div className="container">
+          <div className="section-header text-center mb-3">
+            <h2 className="section-title mb-5">Page not found</h2>
+            <p>Ops... The page you are looking for does not exist.</p>
+          </div>
+        </div>
+      </section>
+  );
+}
+
+export default MainContent;
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,17 +1,11 @@
 import HeroNotFoundPage from "../components/not-found/HeroNotFoundPage";
+import MainContent from "../components/not-found/MainContent";
 import PageLayout from "../layouts/PageLayout";
 
 function NotFoundPage() {
   return (
     <PageLayout hero={<HeroNotFoundPage />}>
-      <section className="help-featured-section theme-section">
-        <div className="container">
-          <div className="section-header text-center mb-5">
-            <h2 className="section-title mb-5">Page not found</h2>
-            <p>Ops... The page you are looking for does not exist.</p>
-          </div>
-        </div>
-      </section>
+      <MainContent />
     </PageLayout>
   );
 }
