4508c4f8d8cae99e979ffa554b1c342e7b6f42a2
joaonunovalente <jnvalente@ua.pt>
2026-02-27 18:10:37 +0000

    [Sobre] Adjust width of MainContainer in smaller screens

--- src/components/sobre/MainContent.jsx
+++ src/components/sobre/MainContent.jsx
@@ -3,57 +3,55 @@ import SectionHeaderTitle from "../common/SectionHeaderTitle";
 function MainContent() {
   return (
     <section>
-      <SectionHeaderTitle title="Contacto" />
+      <SectionHeaderTitle title="Sobre o Cantinas.pt" />
 
-      <div>
-        <div className="row justify-content-center">
-          <div className="col-10">
-            <p>
-              O <strong>Cantinas.pt</strong> é um sítio simples e rápido para
-              consultar toda a informação essencial sobre as cantinas da
-              Universidade de Aveiro.
-            </p>
+      <div className="row justify-content-center">
+        <div className="col-lg-10">
+          <p>
+            O <strong>Cantinas.pt</strong> é um sítio simples e rápido para
+            consultar toda a informação essencial sobre as cantinas da
+            Universidade de Aveiro.
+          </p>
 
-            <p>
-              Inclui informação sobre as ementas das cantinas de Santiago,
-              Crasto, Grelhados, ESTGA e Restaurante Universitário.
-            </p>
+          <p>
+            Inclui informação sobre as ementas das cantinas de Santiago, Crasto,
+            Grelhados, ESTGA e Restaurante Universitário.
+          </p>
 
-            <p>
-              Apresenta os horários de almoço e jantar de cada cantina, bem como
-              os valores das refeições, de forma clara.
-            </p>
+          <p>
+            Apresenta os horários de almoço e jantar de cada cantina, bem como
+            os valores das refeições, de forma clara.
+          </p>
 
-            <br />
+          <br />
 
-            <p>
-              O <strong>Cantinas.pt</strong> apenas facilita o acesso às ementas
-              disponibilizadas pela Universidade de Aveiro.
-              <strong> Não tem qualquer afiliação</strong> com a universidade.
-            </p>
+          <p>
+            O <strong>Cantinas.pt</strong> apenas facilita o acesso às ementas
+            disponibilizadas pela Universidade de Aveiro.
+            <strong> Não tem qualquer afiliação</strong> com a universidade.
+          </p>
 
-            <p>
-              Para entrares em contacto com os Serviços de Ação Social (SAS),
-              responsáveis pelas Unidades Alimentares, utiliza os contactos
-              oficiais:
-            </p>
+          <p>
+            Para entrares em contacto com os Serviços de Ação Social (SAS),
+            responsáveis pelas Unidades Alimentares, utiliza os contactos
+            oficiais:
+          </p>
 
-            <address>
-              <ul>
-                <li>234 370 348</li>
-                <li>sas@ua.pt</li>
-                <li>
-                  <a
-                    href="https://www.ua.pt/pt/sas"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    www.ua.pt/sas
-                  </a>
-                </li>
-              </ul>
-            </address>
-          </div>
+          <address>
+            <ul>
+              <li>234 370 348</li>
+              <li>sas@ua.pt</li>
+              <li>
+                <a
+                  href="https://www.ua.pt/pt/sas"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  www.ua.pt/sas
+                </a>
+              </li>
+            </ul>
+          </address>
         </div>
       </div>
     </section>
