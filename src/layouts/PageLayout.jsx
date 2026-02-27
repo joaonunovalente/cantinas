4f6a686b7d46db384d98da6fcc42731d91b0b95e
joaonunovalente <jnvalente@ua.pt>
2026-02-27 14:19:40 +0000

    [General] Fix footer positioning

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,20 +1,22 @@
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function PageLayout({ children, hero }) {
   return (
     <>
-      <div className="page-header-wrapper">
-        <div className="page-header-bg-pattern-holder">
-          <div className="bg-pattern-top"></div>
-          <div className="bg-pattern-bottom"></div>
+      <div className="site-shell">
+        <div className="page-header-wrapper">
+          <div className="page-header-bg-pattern-holder">
+            <div className="bg-pattern-top"></div>
+            <div className="bg-pattern-bottom"></div>
+          </div>
+          <Navbar />
+          {hero}
         </div>
-        <Navbar />
-        {hero}
-      </div>
 
-      {children}
-      <Footer />
+        {children}
+        <Footer />
+      </div>
     </>
   );
 }
