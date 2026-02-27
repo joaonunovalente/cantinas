b02d729cdd4cc77df5359423792ae990b8074c64
joaonunovalente <jnvalente@ua.pt>
2026-02-27 15:18:56 +0000

    [Mais Informações] Change SideBar title

--- src/components/article/ArticleSidebar.jsx
+++ src/components/article/ArticleSidebar.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
-import sidebarCategories from "../../data/maisInformacoes";
+import maisInformacoes from "../../data/maisInformacoes";
 
 function ArticleSidebar() {
   return (
     <div className="col-lg-8 col-xl-8 mb-2 mb-lg-0">
       <div className="side-bar pt-5 pt-lg-0 sticky">
-        <h3 className="side-bar-heading mb-4">Categories</h3>
+        <h3 className="side-bar-heading mb-4">Mais Informações</h3>
         <nav className="side-bar-nav">
           <ul className="sidebar-nav-items list-unstyled flex-column">
-            {sidebarCategories.map((category) => (
-              <li className="nav-item" key={category.title}>
-                <Link to={category.href}>{category.title}</Link>
+            {maisInformacoes.map((informacao) => (
+              <li className="nav-item" key={informacao.title}>
+                <Link to={informacao.href}>{informacao.title}</Link>
               </li>
             ))}
           </ul>
