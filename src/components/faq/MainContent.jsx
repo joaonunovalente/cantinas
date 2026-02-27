fba3d956bc9d7ee736b19a1861b77843fbdc348f
joaonunovalente <jnvalente@ua.pt>
2026-02-27 10:41:03 +0000

    [FAQ] Remore unsed components from the template

---
+++ src/components/faq/MainContent.jsx
@@ -0,0 +1,14 @@
+import SectionHeaderTitle from "../common/SectionHeaderTitle";
+
+function MainContent() {
+  return (
+    <main className="container">
+      <section className="theme-section">
+        <SectionHeaderTitle title="Perguntas Frequentes" />
+
+      </section>
+    </main>
+  );
+}
+
+export default MainContent;
--- src/pages/FaqPage.jsx
+++ src/pages/FaqPage.jsx
@@ -1,19 +1,12 @@
 import PageLayout from "../layouts/PageLayout";
 
 import Hero from "../components/faq/Hero";
-import CategoriesSection from "../components/faq/CategoriesSection";
-import PopularArticles from "../components/faq/PopularArticles";
-import CtaSection from "../components/faq/CtaSection";
-
-import categories from "../data/categories";
-import popularArticles from "../data/popularArticles";
+import MainContent from "../components/faq/MainContent";
 
 function FaqPage() {
   return (
     <PageLayout hero={<Hero />}>
-      <PopularArticles articles={popularArticles} />
-      <CategoriesSection categoriesList={categories} />
-      <CtaSection />
+      <MainContent />
     </PageLayout>
   );
 }
