c5eaa810c12de56950eab6e70b80ddc54cfeb2a9
joaonunovalente <jnvalente@ua.pt>
2026-02-27 09:05:40 +0000

    [Contacto] Remove unused components from the template

--- src/pages/ContactoPage.jsx
+++ src/pages/ContactoPage.jsx
@@ -1,19 +1,10 @@
 import PageLayout from "../layouts/PageLayout";
 
 import Hero from "../components/contacto/Hero";
-import CategoriesSection from "../components/contacto/CategoriesSection";
-import PopularArticles from "../components/contacto/PopularArticles";
-import CtaSection from "../components/contacto/CtaSection";
-
-import categories from "../data/categories";
-import popularArticles from "../data/popularArticles";
 
 function ContactoPage() {
   return (
     <PageLayout hero={<Hero />}>
-      <PopularArticles articles={popularArticles} />
-      <CategoriesSection categoriesList={categories} />
-      <CtaSection />
     </PageLayout>
   );
 }
