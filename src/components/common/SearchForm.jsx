a089ffed0278de8541e654fd6ea322c531d7bfdb
joaonunovalente <jnvalente@ua.pt>
2026-02-27 18:14:25 +0000

    [Mais Informações] Update SearchFrom placeholder

--- src/components/common/SearchForm.jsx
+++ src/components/common/SearchForm.jsx
@@ -1,7 +1,7 @@
 function SearchForm() {
   return (
     <form className="search-form w-100" onSubmit={(event) => event.preventDefault()}>
-      <input type="text" placeholder="Search the knowledge base..." name="search" className="form-control search-input" />
+      <input type="text" placeholder="Procurar por artigo..." name="search" className="form-control search-input" />
       <button type="submit" className="btn search-btn" value="Search" aria-label="Search">
         <i className="bi bi-search"></i>
       </button>
