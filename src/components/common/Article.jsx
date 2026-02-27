6a069c575eea17215dc48e2239b701d243ba3cee
joaonunovalente <jnvalente@ua.pt>
2026-02-27 17:06:50 +0000

    [Contacto] Adjust margins to aligne Side bar and Main content

--- src/components/common/Article.jsx
+++ src/components/common/Article.jsx
@@ -4,19 +4,20 @@ import ArticleSidebar from "../article/ArticleSidebar";
 function Article({ hero, children }) {
   return (
     <PageLayout hero={hero}>
-      <div className="m-4"></div>
+      <div className="m-3"></div>
+      <main className="container my-5">
+        <div className="help-content-wrapper container mb-3 mb-lg-5">
+          <div className="row gy-5">
+            <div className="col-12 col-lg-2 order-2 order-lg-1">
+              <ArticleSidebar />
+            </div>
 
-      <div className="help-content-wrapper py-5 container">
-        <div className="row gy-5">
-          <div className="col-12 col-lg-2 order-2 order-lg-1 mb-4 mb-lg-0">
-            <ArticleSidebar />
-          </div>
-
-          <div className="col-12 col-lg-8 order-1 order-lg-2">
-            <section>{children}</section>
+            <div className="col-12 col-lg-8 order-1 order-lg-2">
+              <section>{children}</section>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </PageLayout>
   );
 }
